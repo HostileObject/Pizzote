2dcdd1d358fa51aa8664328ef057a073cc7d8de6
HostileObject <samueletonarini@gmail.com>
2021-02-08 19:08:27 +0000

    code format changes

--- pizzote-client/src/components/dataTable.js
+++ pizzote-client/src/components/dataTable.js
@@ -20,268 +20,256 @@ import { connect } from "react-redux";
 import * as actions from "../actions/pizzaData";
 
 function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
+	if (b[orderBy] < a[orderBy]) {
+		return -1;
+	}
+	if (b[orderBy] > a[orderBy]) {
+		return 1;
+	}
+	return 0;
 }
 
 function getComparator(order, orderBy) {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
+	return order === "desc"
+		? (a, b) => descendingComparator(a, b, orderBy)
+		: (a, b) => -descendingComparator(a, b, orderBy);
 }
 
 function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
+	const stabilizedThis = array.map((el, index) => [el, index]);
+	stabilizedThis.sort((a, b) => {
+		const order = comparator(a[0], b[0]);
+		if (order !== 0) return order;
+		return a[1] - b[1];
+	});
+	return stabilizedThis.map((el) => el[0]);
 }
 
 const headCells = [
-  { id: "restaurantName", label: "Restaurant Name" },
-  { id: "entryDate", label: "Date" },
-  { id: "waitTime", label: "Time Waited" },
-  { id: "moment", label: "Moment" },
-  { id: "type", label: "Type" },
-  { id: "price", label: "Price" },
-  { id: "rating", label: "Rating" },
-  { id: "comments", label: "Comments" },
-  { id: "Actions", label: "Actions", disableSorting: true },
+	{ id: "restaurantName", label: "Restaurant Name" },
+	{ id: "entryDate", label: "Date" },
+	{ id: "waitTime", label: "Time Waited" },
+	{ id: "moment", label: "Moment" },
+	{ id: "type", label: "Type" },
+	{ id: "price", label: "Price" },
+	{ id: "rating", label: "Rating" },
+	{ id: "comments", label: "Comments" },
+	{ id: "Actions", label: "Actions", disableSorting: true },
 ];
 
 const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    width: "100%",
-  },
-  table: {
-    minWidth: 650,
-  },
-  container: {
-    width: "80%",
-    margin: "1% auto",
-    backgroundColor: "#222222",
-  },
-  searchInput: {
-    width: "100%",
-    margin: "1%",
-    marginLeft: 0,
-  },
-  addButton: {
-    margin: "1%",
-  },
-  toolbar: {
-    flex: "0 0 80%",
-  },
+	root: {
+		display: "flex",
+		width: "100%",
+	},
+	table: {
+		minWidth: 650,
+	},
+	container: {
+		width: "80%",
+		margin: "1% auto",
+		backgroundColor: "#222222",
+	},
+	searchInput: {
+		width: "100%",
+		margin: "1%",
+		marginLeft: 0,
+	},
+	addButton: {
+		margin: "1%",
+	},
+	toolbar: {
+		flex: "0 0 80%",
+	},
 });
 
 const BlueTableCell = withStyles({
-  root: {
-    color: "#87c9ff",
-    // borderBottom: "1px solid #3a3d3f",
-  },
+	root: {
+		color: "#87c9ff",
+		// borderBottom: "1px solid #3a3d3f",
+	},
 })(TableCell);
 
 const GrayTableCell = withStyles({
-  root: {
-    // color: "#c6c5b6",
-    // borderBottom: "1px solid #3a3d3f",
-  },
+	root: {
+		// color: "#c6c5b6",
+		// borderBottom: "1px solid #3a3d3f",
+	},
 })(TableCell);
 
 function EnhancedTableHead(props) {
-  const { order, orderBy, onRequestSort } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
+	const { order, orderBy, onRequestSort } = props;
+	const createSortHandler = (property) => (event) => {
+		onRequestSort(event, property);
+	};
 
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <BlueTableCell key={headCell.id}>
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-            </TableSortLabel>
-          </BlueTableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
+	return (
+		<TableHead>
+			<TableRow>
+				{headCells.map((headCell) => (
+					<BlueTableCell key={headCell.id}>
+						<TableSortLabel
+							active={orderBy === headCell.id}
+							direction={orderBy === headCell.id ? order : "asc"}
+							onClick={createSortHandler(headCell.id)}
+						>
+							{headCell.label}
+						</TableSortLabel>
+					</BlueTableCell>
+				))}
+			</TableRow>
+		</TableHead>
+	);
 }
 
 function SearchBar(props) {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Toolbar className={classes.toolbar}>
-        <Controls.Input
-          className={classes.searchInput}
-          label="Search"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Search />
-              </InputAdornment>
-            ),
-          }}
-          onChange={props.onChange}
-        />
-      </Toolbar>
-      <Controls.Button
-        className={classes.addButton}
-        text="Add Pizza"
-        startIcon={<AddIcon />}
-        onClick={() => props.setOpenPopup(true)}
-      />
-    </div>
-  );
+	const classes = useStyles();
+	return (
+		<div className={classes.root}>
+			<Toolbar className={classes.toolbar}>
+				<Controls.Input
+					className={classes.searchInput}
+					label="Search"
+					InputProps={{
+						startAdornment: (
+							<InputAdornment position="start">
+								<Search />
+							</InputAdornment>
+						),
+					}}
+					onChange={props.onChange}
+				/>
+			</Toolbar>
+			<Controls.Button
+				className={classes.addButton}
+				text="Add Pizza"
+				startIcon={<AddIcon />}
+				onClick={() => props.setOpenPopup(true)}
+			/>
+		</div>
+	);
 }
 
 function DataTable(props) {
-  const [currentID, setCurrentID] = useState(0);
-  const [recordForEdit, setRecordForEdit] = useState(null);
-  const [openPopup, setOpenPopup] = useState(false);
-  const [order, setOrder] = useState();
-  const [orderBy, setOrderBy] = useState();
-  const [filterFn, setFilterFn] = useState({
-    fn: (items) => {
-      return items;
-    },
-  });
-  const classes = useStyles();
+	const [currentID, setCurrentID] = useState(0);
+	const [recordForEdit, setRecordForEdit] = useState(null);
+	const [openPopup, setOpenPopup] = useState(false);
+	const [order, setOrder] = useState();
+	const [orderBy, setOrderBy] = useState();
+	const [filterFn, setFilterFn] = useState({
+		fn: (items) => {
+			return items;
+		},
+	});
+	const classes = useStyles();
 
-  const rows = props.pizzaDataList;
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
+	const rows = props.pizzaDataList;
+	const handleRequestSort = (event, property) => {
+		const isAsc = orderBy === property && order === "asc";
+		setOrder(isAsc ? "desc" : "asc");
+		setOrderBy(property);
+	};
 
-  const handleSearch = (e) => {
-    let target = e.target;
-    setFilterFn({
-      fn: (items) => {
-        if (target.value == "") return items;
-        else
-          return items.filter((x) =>
-            x.RestaurantName.toLowerCase().includes(target.value)
-          );
-      },
-    });
-  };
+	const handleSearch = (e) => {
+		let target = e.target;
+		setFilterFn({
+			fn: (items) => {
+				if (target.value == "") return items;
+				else return items.filter((x) => x.restaurantName.toLowerCase().includes(target.value));
+			},
+		});
+	};
 
-  const checkID = (rowID) => {
-    if (rowID !== undefined) {
-      return rowID;
-    } else {
-      return "new-entry";
-    }
-  };
+	const checkID = (rowID) => {
+		if (rowID !== undefined) {
+			return rowID;
+		} else {
+			return "new-entry";
+		}
+	};
 
-  const refreshAll = () => {
-    setTimeout(() => {
-      props.getPizzaData();
-    }, 500);
-  };
+	const refreshAll = () => {
+		setTimeout(() => {
+			props.getPizzaData();
+		}, 500);
+	};
 
-  const onDelete = (rowID) => {
-    const onSuccess = () => {
-      console.log("Submitted Succefully");
-    };
-    console.log("deleted");
-    props.deletePizzaData(rowID, onSuccess);
-    refreshAll();
-  };
+	const onDelete = (rowID) => {
+		const onSuccess = () => {
+			console.log("Submitted Succefully");
+		};
+		console.log("deleted");
+		props.deletePizzaData(rowID, onSuccess);
+		refreshAll();
+	};
 
-  const openInPopup = (item) => {
-    setRecordForEdit(item);
-    setCurrentID(item.id);
-    setOpenPopup(true);
-  };
-  return (
-    <>
-      <TableContainer className={classes.container}>
-        <SearchBar onChange={handleSearch} setOpenPopup={setOpenPopup} />
+	const openInPopup = (item) => {
+		setRecordForEdit(item);
+		setCurrentID(item.id);
+		console.log(item.id);
+		setOpenPopup(true);
+	};
+	return (
+		<>
+			<TableContainer className={classes.container}>
+				<SearchBar onChange={handleSearch} setOpenPopup={setOpenPopup} />
 
-        <Table className={classes.table}>
-          <EnhancedTableHead
-            classes={classes}
-            order={order}
-            orderBy={orderBy}
-            onRequestSort={handleRequestSort}
-          />
-          <TableBody>
-            {stableSort(filterFn.fn(rows), getComparator(order, orderBy)).map(
-              (row) => {
-                return (
-                  <TableRow key={checkID(row.id)}>
-                    <GrayTableCell component="th">
-                      {row.restaurantName}
-                    </GrayTableCell>
-                    <GrayTableCell align="left">{row.entryDate}</GrayTableCell>
-                    <GrayTableCell align="left">{row.waitTime}</GrayTableCell>
-                    <GrayTableCell align="left">{row.moment}</GrayTableCell>
-                    <GrayTableCell align="left">{row.type}</GrayTableCell>
-                    <GrayTableCell align="left">{row.price}</GrayTableCell>
-                    <GrayTableCell align="left">{row.rating}</GrayTableCell>
-                    <GrayTableCell align="left">{row.comments}</GrayTableCell>
-                    <GrayTableCell>
-                      <Controls.ActionButton color="primary">
-                        <EditOutlinedIcon
-                          fontSize="small"
-                          onClick={() => openInPopup(row)}
-                        />
-                      </Controls.ActionButton>
-                      <Controls.ActionButton color="secondary">
-                        <CloseIcon
-                          fontSize="small"
-                          onClick={() => onDelete(row.id)}
-                        />
-                      </Controls.ActionButton>
-                    </GrayTableCell>
-                  </TableRow>
-                );
-              }
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Popup
-        title="Pizza Form"
-        openPopup={openPopup}
-        {...{ currentID, setCurrentID }}
-        setOpenPopup={setOpenPopup}
-      >
-        <PizzaForm
-          recordForEdit={recordForEdit}
-          {...{ currentID, setCurrentID }}
-          setOpenPopup={setOpenPopup}
-        />
-      </Popup>
-    </>
-  );
+				<Table className={classes.table}>
+					<EnhancedTableHead
+						classes={classes}
+						order={order}
+						orderBy={orderBy}
+						onRequestSort={handleRequestSort}
+					/>
+					<TableBody>
+						{stableSort(filterFn.fn(rows), getComparator(order, orderBy)).map((row) => {
+							return (
+								<TableRow key={checkID(row.id)}>
+									<GrayTableCell component="th">{row.restaurantName}</GrayTableCell>
+									<GrayTableCell align="left">{row.entryDate}</GrayTableCell>
+									<GrayTableCell align="left">{row.waitTime}</GrayTableCell>
+									<GrayTableCell align="left">{row.moment}</GrayTableCell>
+									<GrayTableCell align="left">{row.type}</GrayTableCell>
+									<GrayTableCell align="left">{row.price}</GrayTableCell>
+									<GrayTableCell align="left">{row.rating}</GrayTableCell>
+									<GrayTableCell align="left">{row.comments}</GrayTableCell>
+									<GrayTableCell>
+										<Controls.ActionButton color="primary">
+											<EditOutlinedIcon fontSize="small" onClick={() => openInPopup(row)} />
+										</Controls.ActionButton>
+										<Controls.ActionButton color="secondary">
+											<CloseIcon fontSize="small" onClick={() => onDelete(row.id)} />
+										</Controls.ActionButton>
+									</GrayTableCell>
+								</TableRow>
+							);
+						})}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<Popup
+				title="Pizza Form"
+				openPopup={openPopup}
+				{...{ currentID, setCurrentID }}
+				setOpenPopup={setOpenPopup}
+			>
+				<PizzaForm
+					recordForEdit={recordForEdit}
+					{...{ currentID, setCurrentID }}
+					setOpenPopup={setOpenPopup}
+				/>
+			</Popup>
+		</>
+	);
 }
 
 const mapStateToProps = (state) => ({
-  pizzaDataList: state.pizzaData.list,
+	pizzaDataList: state.pizzaData.list,
 });
 
 const mapDispatchToProps = {
-  getPizzaData: actions.fetchAll,
-  deletePizzaData: actions.deletePizza,
+	getPizzaData: actions.fetchAll,
+	deletePizzaData: actions.deletePizza,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DataTable);
--- pizzote-client/src/components/pizzaForm.js
+++ pizzote-client/src/components/pizzaForm.js
@@ -7,197 +7,186 @@ import * as actions from "../actions/pizzaData";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles((theme) => ({
-  secondary: {
-    backgroundColor: "#d21f3c",
-    "& .MuiButton-label": {
-      color: "#fff",
-    },
-  },
-  primary: {
-    backgroundColor: "#87c9ff",
-    "& .MuiButton-label": {
-      color: "#232354",
-    },
-  },
+	secondary: {
+		backgroundColor: "#d21f3c",
+		"& .MuiButton-label": {
+			color: "#fff",
+		},
+	},
+	primary: {
+		backgroundColor: "#87c9ff",
+		"& .MuiButton-label": {
+			color: "#232354",
+		},
+	},
 }));
 
 const momentItems = [
-  { id: "launch", title: "Launch" },
-  { id: "dinner", title: "Dinner" },
+	{ id: "launch", title: "Launch" },
+	{ id: "dinner", title: "Dinner" },
 ];
 
 const initialFValues = {
-  id: 0,
-  restaurantName: "",
-  entryDate: new Date().toISOString().slice(0, 10),
-  waitTime: "",
-  moment: "",
-  type: "",
-  price: "",
-  rating: 1,
-  comments: "",
+	id: 0,
+	restaurantName: "",
+	entryDate: new Date().toISOString().slice(0, 10),
+	waitTime: "",
+	moment: "",
+	type: "",
+	price: "",
+	rating: 1,
+	comments: "",
 };
 
 function PizzaForm(props) {
-  const classes = useStyles();
-  useEffect(() => {
-    if (props.currentID != 0) {
-      setValues({ ...props.recordForEdit });
-    }
-  }, [props.currentID]);
+	const classes = useStyles();
+	useEffect(() => {
+		if (props.currentID != 0) {
+			setValues({ ...props.recordForEdit });
+		}
+	}, [props.currentID]);
 
-  const {
-    values,
-    setValues,
-    errors,
-    setErrors,
-    resetForm,
-    handleInputChange,
-  } = useForm(initialFValues);
+	const { values, setValues, errors, setErrors, resetForm, handleInputChange } = useForm(
+		initialFValues
+	);
 
-  const validate = () => {
-    let temp = { ...errors };
-    temp.restaurantName = values.restaurantName
-      ? ""
-      : "This Field is Required.";
-    temp.waitTime = values.waitTime ? "" : "Enter a Valid Time.";
-    temp.moment = values.moment ? "" : "This Field is Required.";
-    temp.type = values.type ? "" : "This Field is Required.";
-    temp.price = values.price ? "" : "Enter a Valid price.";
-    setErrors({
-      ...temp,
-    });
-    return Object.values(temp).every((x) => x == "");
-  };
+	const validate = () => {
+		let temp = { ...errors };
+		temp.restaurantName = values.restaurantName ? "" : "This Field is Required.";
+		temp.waitTime = values.waitTime ? "" : "Enter a Valid Time.";
+		temp.moment = values.moment ? "" : "This Field is Required.";
+		temp.type = values.type ? "" : "This Field is Required.";
+		temp.price = values.price ? "" : "Enter a Valid price.";
+		setErrors({
+			...temp,
+		});
+		return Object.values(temp).every((x) => x == "");
+	};
 
-  const refreshAll = () => {
-    setTimeout(() => {
-      props.getPizzaData();
-    }, 500);
-  };
+	const refreshAll = () => {
+		setTimeout(() => {
+			props.getPizzaData();
+		}, 500);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const onSuccess = () => {
-      console.log("Submitted Succefully");
-    };
-    if (validate()) {
-      if (props.currentID != 0) {
-        props.updatePizzaData(props.currentID, values, onSuccess);
-        refreshAll();
-      } else {
-        props.createPizzaData(values, onSuccess);
-      }
-      props.setCurrentID(0);
-      props.setOpenPopup(false);
-    }
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const onSuccess = () => {
+			console.log("Submitted Succefully");
+		};
+		if (validate()) {
+			if (props.currentID != 0) {
+				props.updatePizzaData(props.currentID, values, onSuccess);
+				refreshAll();
+			} else {
+				props.createPizzaData(values, onSuccess);
+			}
+			props.setCurrentID(0);
+			props.setOpenPopup(false);
+		}
+	};
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Grid container>
-        <Grid item xs={6}>
-          <Controls.Input
-            label="Restaurant Name"
-            name="restaurantName"
-            value={values.restaurantName}
-            onChange={handleInputChange}
-            {...(errors.restaurantName && {
-              error: true,
-              helperText: errors.restaurantName,
-            })}
-          />
-        </Grid>
-        <Grid item xs={3}>
-          <Controls.DatePicker
-            name="entryDate"
-            label="Date"
-            value={values.entryDate}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={3}>
-          <Controls.RadioGroup
-            name="moment"
-            label="Moment"
-            value={values.moment}
-            onChange={handleInputChange}
-            items={momentItems}
-            {...(errors.moment && {
-              error: true,
-              helperText: errors.moment,
-            })}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Controls.Input
-            label="Type"
-            name="type"
-            value={values.type}
-            onChange={handleInputChange}
-            {...(errors.type && {
-              error: true,
-              helperText: errors.type,
-            })}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Controls.Input
-            label="Comments"
-            name="comments"
-            value={values.comments}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Controls.Input
-            label="price"
-            name="price"
-            value={values.price}
-            onChange={handleInputChange}
-            {...(errors.price && {
-              error: true,
-              helperText: errors.price,
-            })}
-          />
-          <Controls.Input
-            label="Time Waited"
-            name="waitTime"
-            value={values.waitTime}
-            onChange={handleInputChange}
-            {...(errors.waitTime && {
-              error: true,
-              helperText: errors.waitTime,
-            })}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Controls.RangeSlider
-            name="rating"
-            value={values.rating}
-            label="rating"
-            onChange={handleInputChange}
-          />
-          <Controls.Button
-            className={classes.primary}
-            type="submit"
-            text="Submit"
-          />
-          <Controls.Button color="default" onClick={resetForm} text="Reset" />
-        </Grid>
-      </Grid>
-    </Form>
-  );
+	return (
+		<Form onSubmit={handleSubmit}>
+			<Grid container>
+				<Grid item xs={6}>
+					<Controls.Input
+						label="Restaurant Name"
+						name="restaurantName"
+						value={values.restaurantName}
+						onChange={handleInputChange}
+						{...(errors.restaurantName && {
+							error: true,
+							helperText: errors.restaurantName,
+						})}
+					/>
+				</Grid>
+				<Grid item xs={3}>
+					<Controls.DatePicker
+						name="entryDate"
+						label="Date"
+						value={values.entryDate}
+						onChange={handleInputChange}
+					/>
+				</Grid>
+				<Grid item xs={3}>
+					<Controls.RadioGroup
+						name="moment"
+						label="Moment"
+						value={values.moment}
+						onChange={handleInputChange}
+						items={momentItems}
+						{...(errors.moment && {
+							error: true,
+							helperText: errors.moment,
+						})}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<Controls.Input
+						label="Type"
+						name="type"
+						value={values.type}
+						onChange={handleInputChange}
+						{...(errors.type && {
+							error: true,
+							helperText: errors.type,
+						})}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<Controls.Input
+						label="Comments"
+						name="comments"
+						value={values.comments}
+						onChange={handleInputChange}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<Controls.Input
+						label="price"
+						name="price"
+						value={values.price}
+						onChange={handleInputChange}
+						{...(errors.price && {
+							error: true,
+							helperText: errors.price,
+						})}
+					/>
+					<Controls.Input
+						label="Time Waited"
+						name="waitTime"
+						value={values.waitTime}
+						onChange={handleInputChange}
+						{...(errors.waitTime && {
+							error: true,
+							helperText: errors.waitTime,
+						})}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<Controls.RangeSlider
+						name="rating"
+						value={values.rating}
+						label="rating"
+						onChange={handleInputChange}
+					/>
+					<Controls.Button className={classes.primary} type="submit" text="Submit" />
+					<Controls.Button color="default" onClick={resetForm} text="Reset" />
+				</Grid>
+			</Grid>
+		</Form>
+	);
 }
 
 const mapStateToProps = (state) => ({
-  pizzaDataList: state.pizzaData.list,
+	pizzaDataList: state.pizzaData.list,
 });
 
 const mapDispatchToProps = {
-  getPizzaData: actions.fetchAll,
-  createPizzaData: actions.create,
-  updatePizzaData: actions.update,
+	getPizzaData: actions.fetchAll,
+	createPizzaData: actions.create,
+	updatePizzaData: actions.update,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PizzaForm);
