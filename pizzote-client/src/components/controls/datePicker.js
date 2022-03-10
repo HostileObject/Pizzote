24c263c90ea201ea1b424cf94b70f1a74ca491d0
stonarini <stonarini@cifpfbmoll.eu>
2022-03-10 20:11:43 +0100

    refactor(datePicker): remove deprecated method and return an empty div

--- pizzote-client/src/components/controls/datePicker.js
+++ pizzote-client/src/components/controls/datePicker.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import DateFnsUtils from '@date-io/date-fns';
+import React from "react";
+import DateFnsUtils from "@date-io/date-fns";
 import {
-    KeyboardDatePicker,
-    MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
 
 export default function DatePicker(props) {
-    const { name, label, value, onChange } = props;
+  const { name, label, value, onChange } = props;
 
-    const convertToDefEventPara = (name, value) => ({
-        target: {
-            name,
-            value: value.toISOString().slice(0, 10),
-        },
-    });
+  const convertToDefEventPara = (name, value) => ({
+    target: {
+      name,
+      value: value.toISOString().slice(0, 10),
+    },
+  });
 
-    return (
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-                disableToolbar
-                variant="inline"
-                inputVariant="outlined"
-                label={label}
-                format="dd/MM/yy"
-                name={name}
-                value={value}
-                onChange={(date) => onChange(convertToDefEventPara(name, date))}
-            />
-        </MuiPickersUtilsProvider>
-    );
+  return (
+    // <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    //     <KeyboardDatePicker
+    //         disableToolbar
+    //         variant="inline"
+    //         inputVariant="outlined"
+    //         label={label}
+    //         format="dd/MM/yy"
+    //         name={name}
+    //         value={value}
+    //         onChange={(date) => onChange(convertToDefEventPara(name, date))}
+    //     />
+    // </MuiPickersUtilsProvider>
+    <div></div>
+  );
 }
